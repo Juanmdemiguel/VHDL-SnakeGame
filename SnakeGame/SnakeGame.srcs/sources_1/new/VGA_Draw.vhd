aec79e5b0837111c86ee6cd8b2ea12d33cf68a21
Juanmdemiguel <juan.mdemiguel@alumnos.upm.es>
2024-12-16 13:23:27 +0100

    Testbench y cambios en vga manager

--- SnakeGame/SnakeGame.srcs/sources_1/new/VGA_Draw.vhd
+++ SnakeGame/SnakeGame.srcs/sources_1/new/VGA_Draw.vhd
@@ -25,14 +25,10 @@ use ieee.numeric_std.all;
 use work.Types.all;
 use work.Letters.all;
 
-entity VGA_Draw is
+entity VGA_DrawGame is
         
     port(
-        --clk_108mhz          : in  std_logic
-        START               : in big_letter_array(0 to 4);
-        GAMEOVER            : in big_letter_array(0 to 8);
         enable              : in std_logic;
-        mode                : in std_logic_vector(1 downto 0);
         snake_length		: in  integer range 0 to snake_length_max;
         snake_mesh_xy		: in  xys(0 to snake_length_max - 1);
         food_xy             : in xy;
@@ -41,7 +37,7 @@ entity VGA_Draw is
         
 end entity;
 
-architecture BEHAVIORAL of VGA_Draw is
+architecture BEHAVIORAL of VGA_DrawGame is
     
      -- Define right top corner of the letter
     constant TEXT_START_X : integer := 500;  -- Horizontal position of the text
@@ -51,56 +47,30 @@ architecture BEHAVIORAL of VGA_Draw is
     
    begin 
    
-control: process(row,col)
-begin
- case mode is
-    when "00" =>
-        typedraw <= '1';
-        Strlen   <= 5;    
-    when "01" =>
-        typedraw <= '0';
-        Strlen   <= 0;
-    when "10" =>
-        typedraw <= '1';
-        Strlen   <= 9;
-    when "11" =>
-        typedraw <= '1';
-        Strlen   <= 5;
-    when others =>
-        typedraw <= '0';
-        Strlen   <= 0;
- end case;
-end process;
-
-drawSnake: process(row, col)
+  Draw: process(row, col)
         --x and y distance from shape
-       -- variable dx, dy     : signed(15 downto 0) := (others => '0');
+         variable dx, dy     : signed(15 downto 0) := (others => '0');
         --if current pixel is shape
         variable is_shape   : std_logic := '0';  
         --if current pixel is food
         variable is_food    : std_logic := '0';
-
-        variable dx, dy : integer := 0;
-        variable is_text_pixel : std_logic := '0';
-        variable selector: integer := 0;
-
+        
     begin
     if enable = '1' then
-        if typeDraw = '0' then 
             --draw body
             is_shape := '0';
             for i in 0 to snake_length_max - 1 loop
                 if (i < snake_length) then  --if is valid snake body
-                    dx := to_integer(abs(signed(col) - signed(snake_mesh_xy(i)(31 downto 16))));
-                    dy := to_integer(abs(signed(row) - signed(snake_mesh_xy(i)(15 downto 0))));
+                    dx := abs(signed(col) - signed(snake_mesh_xy(i)(31 downto 16)));
+                    dy := abs(signed(row) - signed(snake_mesh_xy(i)(15 downto 0)));
                         if (dx < head_width / 2 and dy < head_width / 2) then
                             is_shape := '1';
                         end if;
                  end if;
              end loop;
             
-             dx := to_integer(abs(signed(col) - signed(food_xy(31 downto 16))));
-             dy := to_integer(abs(signed(row) - signed(food_xy(15 downto 0))));
+             dx := abs(signed(col) - signed(food_xy(31 downto 16)));
+             dy := abs(signed(row) - signed(food_xy(15 downto 0)));
              
              if (dx < food_width / 2 and dy < food_width / 2) then
                  is_food := '1';
@@ -108,33 +78,7 @@ drawSnake: process(row, col)
                   is_food := '0';
              end if;
                                 
-        elsif typeDraw = '1' then
-                  -- Calculate relative positions
-                dx := to_integer(unsigned(col)) - TEXT_START_X;
-                dy := to_integer(unsigned(row)) - TEXT_START_Y;
-                -- Check if the pixel is part of the letter
-                is_text_pixel := '0'; -- negated unless said otherwise 
-             --Checks boundaries 
-                   if (dx >= 0 and dx < Strlen*CHAR_LARGE_WIDTH and dy >= 0 and dy < CHAR_LARGE_HEIGHT) then
-                        selector := dx/CHAR_LARGE_WIDTH; --selects the element of the array              
-                    -- Selects the string to print 
-                       if Strlen = 5 then
-                          if START(selector)(dy)(dx - selector*CHAR_LARGE_WIDTH) = '1' then
-                            is_text_pixel := '1';
-                          end if;
-                       elsif Strlen = 9 then
-                          if GAMEOVER(selector)(dy)(dx - selector*CHAR_LARGE_WIDTH) = '1' then
-                            is_text_pixel := '1';
-                          end if;
-                       end if;   
-                   end if;
-            
-            -- Assign colors based on whether it is a text pixel
-                if is_text_pixel = '1' then
-                    rout <= "1111"; -- White text
-                    gout <= "1111";
-                    bout <= "1111";
-                elsif is_shape = '1' then
+                if is_shape = '1' then
                     rout <= "0010";
                     gout <= "0100";
                     bout <= "0110";
@@ -143,11 +87,11 @@ drawSnake: process(row, col)
                     gout <= "1111";
                     bout <= "0000";
                 else
-                    rout <= "0000"; -- Background
-                    gout <= "0100";
-                    bout <= "0000";
+                    rout <= "0100";
+                    gout <= "1100";
+                    bout <= "0100";
                 end if;
-        end if;
+       
         else -- if not enabled
             rout <= "0100";
             gout <= "0000";
--- SnakeGame/SnakeGame.srcs/sources_1/new/VGA_Manager.vhd
+++ SnakeGame/SnakeGame.srcs/sources_1/new/VGA_Manager.vhd
@@ -43,6 +43,8 @@ architecture Behavioral of VGA_Manager is
 
     signal SyncEnable     : std_logic;
     signal row_i, col_i   : std_logic_vector(15 downto 0); 
+    signal red_g,green_g, blue_g : std_logic_vector(3 downto 0); 
+    signal red_s,green_s, blue_s : std_logic_vector(3 downto 0); 
 
  COMPONENT VGA_Sync 
      port (
@@ -53,11 +55,18 @@ architecture Behavioral of VGA_Manager is
      );
 END COMPONENT;
 
-COMPONENT VGA_Draw 
-     port(
+COMPONENT VGA_DrawStr
+  port(
         START               : in big_letter_array(0 to 4);
         GAMEOVER            : in big_letter_array(0 to 8);
+        enable              : in std_logic;
         mode                : in std_logic_vector(1 downto 0);
+        row, col            : in  std_logic_vector(15 downto 0);
+        rout, gout, bout    : out std_logic_vector(3 downto 0)
+        );
+END COMPONENT;
+COMPONENT VGA_DrawGame 
+     port(
         enable              : in std_logic;
         snake_length		: in  integer range 0 to snake_length_max;
         snake_mesh_xy		: in  xys(0 to snake_length_max - 1);
@@ -77,24 +86,52 @@ begin
             col      => col_i
         );
 
-    Inst_VGA_Draw: VGA_Draw 
+    Inst_VGA_DrawGame: VGA_DrawGame 
       PORT MAP (
-        START          => START,
-        GAMEOVER       => GAMEOVER,
-        mode           => mode,
         enable         => SyncEnable,
         snake_length   => snake_length,
         snake_mesh_xy  => snake_mesh_xy,
         food_xy        =>  food_xy,
         row            => row_i, 
         col            => col_i,
-        rout           => red,
-        gout           => green,
-        bout           => blue
+        rout           => red_g,
+        gout           => green_g,
+        bout           => blue_g
     );
---    red <= "1111" when mode = "01" else unaffected;
---    green <= "1111" when mode = "01" else unaffected;
---    blue <= "1111" when mode = "01" else unaffected;
 
+    Inst_VGA_DrawStr : VGA_DrawStr
+      port map(
+        START               => START,
+        GAMEOVER            => GAMEOVER,
+        enable              => SyncEnable,
+        mode                => mode,
+        row                 => row_i,
+        col                 => col_i,
+        rout           => red_s,
+        gout           => green_s,
+        bout           => blue_s
+      );
+
+control: process(clk)
+    begin
+        case mode is
+          when "00" =>
+            red   <= red_s;
+            green <= green_s;
+            blue  <= blue_s;
+          when "01" =>
+            red   <= red_g;
+            green <= green_g;
+            blue  <= blue_g;
+          when "10" =>
+            red   <= red_s;
+            green <= green_s;
+            blue  <= blue_s;
+          when others =>
+            red   <= (others=>'1');
+            green <= (others=>'0');
+            blue  <= (others=>'0');
+        end case;      
+end process;
 
 end Behavioral;
